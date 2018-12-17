914b24a30b0ad1a9fe4125e521d2fbe4f7c46f02
Kevin Chin <chinkevin25@gmail.com>
2018-12-16 21:46:07 -0800

    fix some css

--- src/components/description.js
+++ src/components/description.js
@@ -5,9 +5,9 @@ const Details = ({movie, handleWatched}) => {
 
   return (
     <div className='description'>
+      <div>Description: {movie.overview}</div>
       <div>Year: {movie.release_date}</div>
-      <div>IMDB Rating {movie.vote_average}</div>
-      <div>Description {movie.overview}</div>
+      <div>Rating: {movie.vote_average}</div>
       <button className="watch" data-movie={movie.title} onClick = {event => handleWatched(event)} >{movie.watched ? 'Watched' : 'To Watch'}</button>
     </div>
   );
