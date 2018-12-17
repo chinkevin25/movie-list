1c80ffdaa9b537c1ef223c16db257f4ddee85a84
Kevin Chin <chinkevin25@gmail.com>
2018-12-16 21:38:01 -0800

    implement render from db

--- src/components/description.js
+++ src/components/description.js
@@ -5,14 +5,12 @@ const Details = ({movie, handleWatched}) => {
 
   return (
     <div className='description'>
-      <div>Year</div>
-      <div>Run Time</div>
-      <div>Metascore</div>
-      <div>imbd Rating</div>
-      <img src='https://i.imgur.com/pdUySbA.jpg'/>
+      <div>Year: {movie.release_date}</div>
+      <div>IMDB Rating {movie.vote_average}</div>
+      <div>Description {movie.overview}</div>
       <button className="watch" data-movie={movie.title} onClick = {event => handleWatched(event)} >{movie.watched ? 'Watched' : 'To Watch'}</button>
     </div>
-  )
+  );
 }
 
 export default Details;
